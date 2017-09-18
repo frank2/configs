f0c3b2b75c888c868f878d9082b377d407166007
frank2 <frank2@dc949.org>
2017-09-18 13:22:03 -0700

    make everything lowercase

--- update.sh
+++ update.sh
@@ -7,7 +7,7 @@ if [ ! -d "$CONFIGS" ]; then
     exit 1
 fi
 
-echo "[+] Updating configs" 1>&2
+echo "[+] updating configs" 1>&2
 
 pushd "$CONFIGS"
 git pull origin master || ( echo "update: git failed" 1>&2 && exit 1 )
@@ -19,10 +19,10 @@ fi
 
 popd
 
-echo "[+] Repository updated. Running installer." 1>&2
+echo "[+] repository updated. running installer." 1>&2
 
 "$CONFIGS/install.sh" || ( echo "update: installer failed" 1>&2 && exit 1 )
 
-echo "[+] Configs updated." 1>&2
+echo "[+] configs updated." 1>&2
 
 exit 0
